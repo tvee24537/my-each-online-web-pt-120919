12b74a6478b1e901406e6c1c042eea2da9a0622c
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-20 21:00:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
-    if block_given?
     i = 0
  
     while i < array.length
       yield(array[i])
       i = i + 1
     end
- 
     array
-  else
-    puts "Hey! No block was given!"
-  end
-end
 end
